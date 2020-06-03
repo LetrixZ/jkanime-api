ef960cd75bfbd99ef9fb05b1960f3b829c52d274
Fermin Cirella <letrix@outlook.es>
2020-06-03 16:17:40 -0300

    fixes

--- app.py
+++ app.py
@@ -12,42 +12,80 @@ def return_json(obj):
     response = app.response_class(json.dumps(obj, sort_keys=False), mimetype=app.config['JSONIFY_MIMETYPE'])
     return response
 
-@app.route('/video/<string:id>/<int:chapter>')
-def getVideoByAnimeId(id, chapter):
-    start_time = time.time()
-    driver.get('https://jkanime.net/{}/{}'.format(id, chapter))
-    player_url = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[1]/div[2]/div[1]/center/div/div/iframe').get_attribute('src')
+def getEpisodeVideo(id, chapter):
+    page = requests.get('https://jkanime.net/{}/{}'.format(id, chapter))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    scripts = soup.findAll('script')
+    vid_script = ''
+    for script in scripts:
+        if 'var video = [];' in str(script):
+            vid_script = str(script)
+    vid_script = vid_script.replace('  ', '')
+    vid_script = vid_script[23:vid_script.find('var video_data = video')].split('video')
+    src = vid_script[1]
+    player_url = src[src.find('src=')+5:src.find(' width')-1]
+    #driver.get('https://jkanime.net/{}/{}'.format(id, chapter))
+    #player_url = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[1]/div[2]/div[1]/center/div/div/iframe').get_attribute('src')
     driver.get(player_url)
     video = driver.find_element_by_xpath('/html/body/div[1]/video/source').get_attribute('src')
-    print("--- %s seconds ---" % (time.time() - start_time))
-    return return_json({'video':video})
+    return video
 
-@app.route('/info/<string:id>')
-def getAnimeInfoById(id):
-    start_time = time.time()
+def getAnimeInfo(id):
     page = requests.get('https://jkanime.net/{}'.format(id))
     body = BeautifulSoup(page.content, 'html.parser')
     info = body.findAll('div', {'class':'info-field'})
-    anime = {}
-    anime['name'] = body.find('div', {'class':'info-content'}).find('h2').getText()
-    anime['poster'] = body.findAll('img')[1].get('src')
-    anime['type'] = info[0].find('span', {'class':'info-value'}).getText()
-    anime['synopsis'] = info[7].find('p').getText()[10:]
+    name = body.find('div', {'class':'info-content'}).find('h2').getText()
+    poster = body.findAll('img')[1].get('src')
+    type = info[0].find('span', {'class':'info-value'}).getText()
+    synopsis = info[7].find('p').getText()[10:-1]
     genres = info[1].find('span', {'class':'info-value'}).findAll('a')
     tmpList = []
     for genre in genres:
         tmpList.append(genre.getText())
-    anime['genres'] = tmpList
-    anime['episodes'] = info[3].find('span', {'class':'info-value'}).getText()
-    anime['duration'] = info[4].find('span', {'class':'info-value'}).getText()[:info[4].find('span', {'class':'info-value'}).getText().find(' p')]
+    #episodes = info[3].find('span', {'class':'info-value'}).getText()
+    episodeText = body.find('div', {'class':'navigation'}).findAll('a')[-1].getText()
+    episodes = episodeText[episodeText.find("- ")+2:]
+    duration = info[4].find('span', {'class':'info-value'}).getText()[:info[4].find('span', {'class':'info-value'}).getText().find(' p')]
     date =  info[5].find('span', {'class':'info-value'}).getText().replace('  ', '').replace('\n','')
-    anime['startDate'] = date
-    if date.find(' a '):
-        anime['startDate'] = date[:date.find(' a ')]
-        anime['finishDate'] = date[date.find(' a ')+3:]
-    anime['state'] = info[6].find('span', {'class':'info-value'}).find('b').getText()
+    startDate = date
+    finishDate = None
+    state = info[6].find('span', {'class':'info-value'}).find('b').getText()
+    if 'Concluido' in state:
+        startDate = date[:date.find(' a ')]
+        finishDate = date[date.find(' a ')+3:]
+    return (name, poster, type, synopsis, tmpList, episodes, duration, startDate, finishDate, state)
+
+@app.route('/video/<string:id>/all')
+def getVideosByAnimeId(id):
+    start_time = time.time()
+    episodes = int(getAnimeInfo(id)[5])
+    videos = []
+    for i in range(episodes):
+        videoDict = {}
+        video = getEpisodeVideo(id, i+1)
+        videoDict['episode'] = str(i+1)
+        videoDict['url'] = video
+        videos.append(videoDict)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return return_json({'videos':videos})
+
+@app.route('/video/<string:id>/<int:chapter>')
+def getVideoByAnimeId(id, chapter):
+    start_time = time.time()
+    video = getEpisodeVideo(id, chapter)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return return_json({'video':video})
+
+
+
+@app.route('/info/<string:id>')
+def getAnimeInfoById(id):
+    start_time = time.time()
+    info = getAnimeInfo(id)
+    anime = {'name':info[0],'poster':info[1],'type':info[2],'synopsis':info[3],'genres':info[4],'episodes':info[5],'duration':info[6],'startDate':info[7],'finishDate':info[8],'state':info[9]}
     print("--- %s seconds ---" % (time.time() - start_time))
     return return_json(anime)
+
 """
 @app.route('/search/<string:name>')
 def searchAnime(name):
