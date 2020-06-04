0c581954a699f6904bcbdd86edc1708b0e197715
Fermin Cirella <letrix@outlook.es>
2020-06-04 14:39:33 -0300

    fixes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os, time, requests, asyncio, aiohttp, re
 from flask import Flask, json
-#from init import driver
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['JSON_SORT_KEYS'] = False
 app.config['JSON_AS_ASCII'] = False
 
-def return_json(obj):
+def returnJson(obj):
     response = app.response_class(json.dumps(obj, sort_keys=False), mimetype=app.config['JSONIFY_MIMETYPE'])
     return response
 
@@ -25,8 +24,6 @@ def getEpisodeVideo(id, chapter):
     vid_script = vid_script[23:vid_script.find('var video_data = video')].split('video')
     src = vid_script[1]
     player_url = src[src.find('src=')+5:src.find(' width')-1]
-    #driver.get('https://jkanime.net/{}/{}'.format(id, chapter))
-    #player_url = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[1]/div[2]/div[1]/center/div/div/iframe').get_attribute('src')
     driver.get(player_url)
     video = driver.find_element_by_xpath('/html/body/div[1]/video/source').get_attribute('src')
     return video
@@ -42,7 +39,6 @@ def getAnimeInfo(body):
     tmpList = []
     for genre in genres:
         tmpList.append(genre.getText())
-    #episodes = info[3].find('span', {'class':'info-value'}).getText()
     episodeText = body.find('div', {'class':'navigation'}).findAll('a')[-1].getText()
     episodes = episodeText[episodeText.find("- ")+2:]
     duration = info[4].find('span', {'class':'info-value'}).getText().replace('.','')
@@ -75,14 +71,14 @@ def getVideosByAnimeId(id):
         videoDict['url'] = video
         videos.append(videoDict)
     print("--- %s seconds ---" % (time.time() - start_time))
-    return return_json({'videos':videos})
+    return returnJson({'videos':videos})
 
 @app.route('/video/<string:id>/<int:chapter>')
 def getVideoByAnimeId(id, chapter):
     start_time = time.time()
     video = getEpisodeVideo(id, chapter)
     print("--- %s seconds ---" % (time.time() - start_time))
-    return return_json({'video':video})
+    return returnJson({'video':video})
 
 @app.route('/info/<string:id>')
 def getAnimeInfoById(id):
@@ -90,7 +86,7 @@ def getAnimeInfoById(id):
     info = getAnimeInfo(getBody(id))
     anime = {'name':info[0],'poster':info[1],'type':info[2],'synopsis':info[3],'genres':info[4],'episodes':info[5],'duration':info[6],'startDate':info[7],'finishDate':info[8],'state':info[9]}
     print("--- %s seconds ---" % (time.time() - start_time))
-    return return_json(anime)
+    return returnJson(anime)
 
 def search(name, page):
     page = requests.get('https://jkanime.net/buscar/{}/{}'.format(name, page))
@@ -127,7 +123,7 @@ def searchAnime(name):
         info = getData(entry)
         anime = {'id':info[0], 'name':info[1],'poster':info[2],'type':info[3],'synopsis':info[4],'episodes':info[5],'state':info[6]}
         animes.append(anime)
-    return return_json(animes)
+    return returnJson(animes)
 
 def getBodies(urlList):
     bodies = []
@@ -136,7 +132,7 @@ def getBodies(urlList):
             async with aiohttp.ClientSession() as session:
                 async with session.get(url=url) as response:
                     resp = await response.read()
-                    print("Successfully got url {} with response of length {}.".format(url, len(resp)))
+                    #print("Successfully got url {} with response of length {}.".format(url, len(resp)))
                     if len(resp) > 1000:
                         bodies.append(resp)
         except Exception as e:
@@ -172,17 +168,17 @@ def getSchedule(day):
 @app.route('/schedule/<int:day>')
 def getScheduleByDay(day):
     if day > 7 or day < 1:
-        return return_json({'schedule':''})
-    return return_json(getSchedule(day))
+        return returnJson({'schedule':''})
+    return returnJson(getSchedule(day))
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return return_json({'message':'404 - page not found'})
+    return returnJson({'message':'404 - page not found'})
 
 @app.route("/")
 def index():
     default_dict = {"message" : "🏴‍☠️🦜"}
-    return return_json(default_dict)
+    return returnJson(default_dict)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
